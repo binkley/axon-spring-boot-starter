33e89758bd17a4b17481c1fd08747cf74a0ad2ae
Brian Oxley <boxley@thoughtworks.com>
2016-02-24 23:20:32 -0500

    Better code.

--- axon-spring-boot-starter-clustering-eventbus/src/main/java/hm/binkley/spring/axon/EventProcessingMonitorAutomation.java
+++ axon-spring-boot-starter-clustering-eventbus/src/main/java/hm/binkley/spring/axon/EventProcessingMonitorAutomation.java
@@ -6,15 +6,23 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.event.ContextRefreshedEvent;
 
+import java.util.Collection;
+
+import static org.springframework.beans.factory.BeanFactoryUtils
+        .beansOfTypeIncludingAncestors;
+
 public class EventProcessingMonitorAutomation
         implements ApplicationListener<ContextRefreshedEvent> {
     @Override
     public void onApplicationEvent(final ContextRefreshedEvent event) {
         final ConfigurableApplicationContext context
                 = (ConfigurableApplicationContext) event.getSource();
-        for (final Cluster cluster : context.getBeansOfType(Cluster.class)
-                .values())
-            context.getBeansOfType(EventProcessingMonitor.class).values().
-                    forEach(cluster::subscribeEventProcessingMonitor);
+        final Collection<EventProcessingMonitor> monitors
+                = beansOfTypeIncludingAncestors(context.getBeanFactory(),
+                EventProcessingMonitor.class).values();
+
+        for (final Cluster cluster : beansOfTypeIncludingAncestors(
+                context.getBeanFactory(), Cluster.class).values())
+            monitors.forEach(cluster::subscribeEventProcessingMonitor);
     }
 }
